2dc648e2d0e6e03db466d7ba5abb2e706c5cc9d1
Lucas Rocha <lucas_santos239@outlook.com>
2025-03-26 11:37:14 -0300

    Create atividade_1.py

---
+++ Python/Exercicios/atividade_1.py
@@ -0,0 +1,5 @@
+number_1 = int(input("Escreva o primeiro número: "))
+number_2 = int(input("Escreva o segundo número: "))
+resultado = number_1 + number_2
+
+print("A soma de ", number_1, "+", number_2, "=", resultado)
