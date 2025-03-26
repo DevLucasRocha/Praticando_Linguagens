6ec7ec9b573f15f8aa2a2bb1d17a4fb7b4ffe069
Lucas Rocha <lucas_santos239@outlook.com>
2025-03-26 11:45:27 -0300

    Update atividade_1.py

--- Python/Exercicios/atividade_1.py
+++ Python/Exercicios/atividade_1.py
@@ -1,5 +1,21 @@
-number_1 = int(input("Escreva o primeiro número: "))
-number_2 = int(input("Escreva o segundo número: "))
-resultado = number_1 + number_2
+#Código inicial:
 
-print("A soma de ", number_1, "+", number_2, "=", resultado)
+"""number_1 = int(input("Escreva o primeiro número: "))
+number_2 = int(input("Escreva o segundo número: "))
+
+resultado = number_1 + number_2
+print("A soma de", number_1, "+", number_2, "=", resultado)"""
+
+
+#Código corrigido e melhorado fazendo o uso de try-except:
+
+try:
+    number_1 = int(input("Escreva o primeiro número: "))
+    number_2 = int(input("Escreva o segundo número: "))
+
+    resultado = number_1 + number_2
+
+    print(f'A soma de {number_1} + {number_2} = {resultado}')
+except ValueError:
+    print("Erro: Por favor, insira apenas números inteiros.")
+    
