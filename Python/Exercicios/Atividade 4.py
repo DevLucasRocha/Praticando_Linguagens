7806bef081000cc7fae018456abe224f203a02be
Lucas Rocha <lucas_santos239@outlook.com>
2025-03-28 21:03:11 -0300

    Update Atividade 4.py

--- Python/Exercicios/Atividade 4.py
+++ Python/Exercicios/Atividade 4.py
@@ -7,13 +7,16 @@ numeros_extenso = (
 
 while True:
     num = int(input("Digite um número entre 0 e 20: "))
-    if 0 <= num <= 20:
-          break
+    if num < 0 or num > 20:  # Verifica se o número está fora do intervalo
+        print("Tente novamente. ", end='')  # Não pula linha
     else:
-         print("Tente novamente. ", end='') # end="" - ele não vai pular a linha e vai juntar com a próxima escrita
-print(f"Você digitou o número {numeros_extenso[num]}")
-
-
+        print(f"Você digitou o número {numeros_extenso[num]}")
+    
+    loop = input("Quer continuar? sim/não\n").strip().lower()
+    if loop in "n":
+        break  # Sai do loop
+    else:
+        continue  # Continua o loop
         
     
 
