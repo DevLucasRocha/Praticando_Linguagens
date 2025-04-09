499155f97e42a8f04a8e3f8d175d884d7c0f1042
Lucas Rocha <lucas_santos239@outlook.com>
2025-04-09 15:27:39 -0300

    Update Atividade 6.py

--- Linguagens/Python/Atividade 6.py
+++ Linguagens/Python/Atividade 6.py
@@ -6,12 +6,16 @@ import os
 import sys
 print("Sorteando números aleatórios")
 print("-"*30)
-print("Gerando 10 números aleatórios entre 1 e 10")
+print("Gerando 5 números aleatórios entre 1 e 10")
 print("-"*30)
 time.sleep(2)
-for i in range(10): # Gera 10 números aleatórios
-    print(random.randint(1,10)) # Gera números aleatórios entre 1 e 10
+for i in range(5): # Gera 5 números aleatórios
+    print(random.randint(1,5)) # Gera números aleatórios entre 1 e 10
     time.sleep(1)
 print("-"*30)
+print("Maior número sorteado: ", end="")
+os.system("cls") # Limpa a tela
+print(f"menor número sorteado:{i}", end="") 
+os.system("cls") # Limpa a tela
 print("Fim do programa")
 print("-"*30)
